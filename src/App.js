6b426017e42ebe1f9dcfa6fd839c9e81fef55139
Your Name <you@example.com>
2021-04-23 15:42:10 +0530

    contacts part done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
-import Contact from './Components/Contact';
+//import Contact from './Components/Contact';
 import Portfolio from './Components/Portfolio';
 import ParticlesBg  from "particles-bg";
 import { ParallaxProvider } from 'react-scroll-parallax';
@@ -55,8 +55,10 @@ class App extends Component {
             <li><a className="smoothscroll" href="#about" style={{color:'rgb(108, 245, 66)'}}>About</a></li>
 	         <li><a className="smoothscroll" href="https://tarun-sharma27.github.io/Arts-club/single.html" style={{color:'rgb(108, 245, 66)'}}>Gallery</a></li>
             <li><a className="smoothscroll" href="#portfolio" style={{color:'rgb(108, 245, 66)'}}>Events</a></li>
-            <li><a className="smoothscroll" href="#contact" style={{color:'rgb(108, 245, 66)'}}>Contact</a></li>
+            {/* <li><a className="smoothscroll" href="#contact" style={{color:'rgb(108, 245, 66)'}}>Contact</a></li> */}
             <li><a className="smoothscroll" href="#resume" style={{color:'rgb(108, 245, 66)'}}>Team</a></li>
+            <li><a className="smoothscroll" href="tel:8887528024" style={{color:'rgb(108, 245, 66)'}}><i class="fa fa-phone fa-2x" aria-hidden="true"></i></a></li>
+            <li><a className="smoothscroll" href="mailto:2019uce0040@iitjammu.ac.in" style={{color:'rgb(108, 245, 66)'}}><i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a></li>
             <li><button style={{height:40,  width: 40,  textAlign:'right', backgroundColor: 'blue', margin:10, borderRadius: '50%',display: 'inline-block'}} onClick={this.clickColor1}></button></li>
             <li><button style={{height:40,  width: 40,  textAlign:'right', backgroundColor: 'cyan', margin:10,  borderRadius: '50%',display: 'inline-block'}} onClick={this.clickColor2}></button> </li>
             <li><button style={{height:40,  width: 40,  textAlign:'right', backgroundColor: 'green',  margin:10, borderRadius: '50%',display: 'inline-block'}} onClick={this.clickColor3}></button></li>
@@ -68,7 +70,7 @@ class App extends Component {
         <About colorinfo={this.state.color2} />
         <Resume colorinfo={this.state.color3} />
         <Portfolio colorinfo={this.state.color4} />
-        <Contact colorinfo={this.state.color5} />
+        {/* <Contact colorinfo={this.state.color5} /> */}
         <Footer colorinfo={this.state.color6} />
       </div>
     );
